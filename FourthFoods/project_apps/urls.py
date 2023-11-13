a066e222fd359f5765cddcb280148d9de4ba9f4a
Daniel <dc20643@georgiasouthern.edu>
2023-11-13 13:50:20 -0500

    Adding url connectivity

--- FourthFoods/project_apps/urls.py
+++ FourthFoods/project_apps/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'project_apps'
+
 urlpatterns = [
-    path('', views.home_view)
+    path('', views.home_view, name="home"),
+    path('features/', views.features_view, name="features"),
+    path('about/', views.about_view, name="about")
+
 ]
--- FourthFoods/project_apps/views.py
+++ FourthFoods/project_apps/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpRequest
 
 # Create your views here.
 def home_view(request):
     return render(request, 'project_apps/home.html')
+
+def features_view(request):
+        return render(request, 'project_apps/features.html')
+
+def about_view(request):
+        return render(request, 'project_apps/about.html')
